193ccfc50e7b3e93c621f3c94c58bd747eb03056
MordredGit <mshagun2001@gmail.com>
2023-11-28 02:13:22 +0530

    patch: / redirects to /app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { browser } from '$app/environment';
+
+	if (browser) {
+		// to prevent error window is not defined, because it's SSR
+		window.location.href = '/app';
+	}
+</script>
